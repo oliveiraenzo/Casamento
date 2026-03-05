3ec2031aef598120ff90e77ca2ddff51172f5f94
Enzo Oliveira <enzobrito2003@hotmail.com>
2026-03-04 21:59:22 -0300

    Att

--- public/rsvp.js
+++ public/rsvp.js
@@ -1,18 +1,13 @@
 // --- LISTA DE CONVIDADOS ---
 const guestList = [
-    "João da Silva",
-    "Maria Oliveira",
-    "Carlos Eduardo",
-    "Ana Clara Souza",
-    "Pedro Henrique",
-    "Enzo Brito",
     /* Ana */
     "Janira Bulhões",
     "Brazilina Bulhões",
+    "João Bulhões",
+    "Maria Eduarda Scagnolato",
     "Anne Caroline Bulhões",
     "Fernanda Ribeiro",
-    "Caroline Rodrigues",
-    "Leticia Barbosa",
+    "Letícia Barbosa",
     "Keth Oliveira",
     "Joice Fernandes",
     "Kemily Morelato",
@@ -20,16 +15,14 @@ const guestList = [
     "Jeniffer Soares",
     "Karoline Lima",
     "Ellen Oliveira",
-    "João Bulhões",
-    "Josira Bulhões",
     "Lincoln Oliveira",
     "Alexandre Alves",
     "Adiel Santos",
-    "Guilherme Santos",
+    "Guilherme Ferreira",
     "Cleyton Pereira",
     "Giulia Mirela Fernandes",
     "Daniel Sanches",
-    "Flavio Limaa",
+    "Flavio Lima",
     "Alex Oliveira",
     "Thainara Volponi",
     "Kaique Silvestre",
@@ -40,17 +33,42 @@ const guestList = [
     "Jessica Mattos",
     "Kalina Tokuda",
     "Yuri Tokuda",
-    "Luiz Henrique",
-    "Rute Ferreira",
-    "Fabiana Olegário",
+    "Fabiana Olegario",
     "Lázaro Nery",
     "Stefany Naciso",
-    "Matheus*",
-    "Bianca Oliveira",
-    "Eduardo Oliveira",
+    "Matheus Santos",
+    "Rute Ferreira",
+    "Luiz Henrique",
     "Gilnália Carneiro",
     "Francisco Carneiro",
+    "Lany Luz",
+    "Mariana Paiva",
+    "Josira Bulhões",
+    "Poliana Bulhões",
+    "Maria Bulhões",
+    "Marcos Bulhões",
+    "Daiana Bulhões",
+    "Éverton Bulhões",
+    "Juscelino Bulhões",
+    "Leda Bulhões",
+    "Livia Bulhões",
+    "Carlos Bulhões",
+    "Jucilene Bulhões",
+    "Jailton Bulhões",
+    "Yasca Bulhões",
+    "Jean Bulhões",
+    "Márcia Bulhões",
+    "Rafaela Bulhões",
+    "Nathalia Bulhões",
+    "Thaís Bulhões",
+    "Gabriel Bulhões",
+    "Janete Bulhões",
+    "Iran Bulhões",
+    "Bianca Oliveira",
+    "Eduardo Oliveira",
+    "Amos Bulhões",
     "Maria Eduarda Fernandes",
+    "Pedro Jorge Novaes",
     "Lucca Barboza",
     "Lorena Silva",
     "Laura Oliveira",
@@ -60,7 +78,107 @@ const guestList = [
     "Davi Tokuda",
     "Lucca Tokuda",
     "Giovana Oliveira",
-    "Davi Carneiro"
+    "Bianca Bulhões",
+    "Davi Carneiro",
+    "Pietra Bulhões",
+    "Luna Bulhões",
+    //Eto
+    "Marcia da Silva",
+    "Everson Alves",
+    "Enzo Brito",
+    "Edna Rodrigues",
+    "Marcos Conceição",
+    "Maria de Jesus",
+    "Sergio Gasperini",
+    "Deise Naira",
+    "Cristiano Angelo",
+    "Ryan Rodrigues",
+    "Emerson Rodrigues",
+    "Roberta Pereira",
+    "Vinicius Pereira",
+    "Marizete Pereira",
+    "Paulo Rex",
+    "Carla Poliana",
+    "Alexandro Ribeiro",
+    "Camila Pazielly",
+    "Geverson Souza",
+    "Gabriel Lacerda",
+    "Camila Mello",
+    "Tatiane Lacerda",
+    "Eliesio Borges",
+    "Lara Karoline",
+    "Thiago Borges",
+    "Bernadete Pereira",
+    "Aretuza Pereira",
+    "Marcelo Estevan",
+    "Marcelo Júnior",
+    "William Pedro",
+    "Douglas Pedro",
+    "Camila Cândido",
+    "Leonardo Pereira",
+    "Andressa Oliveira",
+    "Tereza Vieira",
+    "José Arnaldo",
+    "Marcos Pereira",
+    "Tuany Pereira",
+    "Jozeli Pereira",
+    "Gilberto Oliveira",
+    "Aline Oliveira",
+    "Wesley Bozelli",
+    "Vanessa Cristina",
+    "Emily Nadir",
+    "Mateus Oliveira",
+    "Alexia Karine",
+    "Richard Furlan",
+    "Emerson Oliveira",
+    "Claudia Oliveira",
+    "Victor Hugo",
+    "Vitória Oliveira",
+    "Felipe Caio",
+    "Victor Vinícius",
+    "Cauê Tântalo",
+    "Esposa Cauê",
+    "Priscila Rosini",
+    "Doug Sdorf",
+    "Priscila Lourenço",
+    "Gabriel Luiz",
+    "Marco Aurélio",
+    "Andréa Silva",
+    "Vicente Feola",
+    "Paula Feola",
+    "Paulo Santana",
+    "Thiago Murilo Toot",
+    "Rogério Santos",
+    "Nayara Brito",
+    "Adilson",
+    "Lucia",
+    "Adilson Junior",
+    "William Cabral",
+    "Tainan Santos",
+    "Jader Fernandes",
+    "Marilaine Aparecida",
+    "Gustavo Tozzo",
+    "Ananda Tozzo",
+    "Alexander Oliveira",
+    "Thalita Lauzen",
+    "Uriel Fraire",
+    "Emmanuel Ponce",
+    "Maria Júlia",
+    "Arthur Felipe",
+    "Maria Eduarda",
+    "Anne Caroline",
+    "Julia",
+    "Luísa",
+    "Heloísa",
+    "Lucca",
+    "Luan",
+    "Vicenzo",
+    "Isabela",
+    "Helena Fernandes",
+    "Kelvin",
+    "Bernardo",
+    "Pierre",
+    "Allan Jhones",
 ];
 
 // Variável para controlar o tempo de espera (Debounce)
@@ -79,7 +197,7 @@ function normalizeString(str) {
 // Inicializa o evento de escuta no input assim que a página carrega
 document.addEventListener('DOMContentLoaded', () => {
     const input = document.getElementById('guestName');
-    
+
     // O evento 'input' dispara a cada letra digitada
     input.addEventListener('input', () => {
         checkGuestRealTime();
@@ -112,7 +230,7 @@ function checkGuestRealTime() {
         input.blur(); // Tira o foco do teclado (útil em celular)
         stepInput.classList.add('hidden');
         stepConfirm.classList.remove('hidden');
-        welcomeMsg.textContent = `Olá, ${found}!`; 
+        welcomeMsg.textContent = `Olá, ${found}!`;
     } else {
         // ERRO COM DELAY: Se não achou, espera o usuário parar de digitar
         typingTimer = setTimeout(() => {
@@ -126,10 +244,10 @@ function reply(isConfirmed) {
     const stepConfirm = document.getElementById('step-confirm');
     const stepFinal = document.getElementById('step-final');
     const finalMsg = document.getElementById('final-msg');
-    
+
     // 1. PEGA O NOME DO CONVIDADO ANTES DE MUDAR A TELA
     const input = document.getElementById('guestName');
-    const guestName = input.value; 
+    const guestName = input.value;
 
     // 2. CHAMA A FUNÇÃO PARA SALVAR NO EXCEL (SheetDB)
     sendToSheet(guestName, isConfirmed);
@@ -165,7 +283,7 @@ function goHome() {
 
     // 4. Mostra a tela inicial
     stepInput.classList.remove('hidden');
-    
+
     // 5. Opcional: foca no input para digitar
     setTimeout(() => input.focus(), 100);
 }
@@ -173,14 +291,14 @@ function goHome() {
 function confirmAgain() {
     const input = document.getElementById('guestName');
     const feedback = document.getElementById('feedback-text');
-    
+
     // Telas
     const stepInput = document.getElementById('step-input');
     const stepFinal = document.getElementById('step-final');
 
     // 1. Limpa o input
     input.value = "";
-    
+
     // 2. Garante que erros estejam ocultos
     feedback.classList.add('hidden');
 
@@ -209,6 +327,6 @@ function sendToSheet(name, status) {
         },
         body: JSON.stringify({ data: data })
     })
-    .then(response => console.log('Salvo na planilha!'));
+        .then(response => console.log('Salvo na planilha!'));
 }
 
